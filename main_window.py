e0afc3453b03c2218f7f1c3fc68602062f0222c6
Adam-Howe <40769@longroad.ac.uk>
2015-11-26 12:17:07 +0000

    code

--- main_window.py
+++ main_window.py
@@ -3,20 +3,11 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-from client_window import *
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__() #super class constructor
         self.setWindowTitle("Stock Control System")
         self.create_layout()
-
-        #self.grid_layout = QGridLayout()
-        #self.grid_layout.addWidget(self.select_widget)
-
-        #self.central_widget = QWidget()
-        #self.central_widget.setLayout(self.grid_layout)
-        #self.setCentralWidget(self.central_widget)
 
         self.stacked_layout = QStackedLayout()
         self.stacked_layout.addWidget(self.select_widget)
@@ -51,42 +42,159 @@ class MainWindow(QMainWindow):
         self.password_button.clicked.connect(self.change_password)
 
     def create_client_layout(self):
-        #second layout
         self.view_button = QPushButton("View Full Database")
         self.search_button = QPushButton("Search")
         self.edit_button = QPushButton("Edit An Entry")
+        self.back_button = QPushButton("Back")
 
         self.search_line_edit = QLineEdit()
         self.edit_line_edit = QLineEdit()
 
         self.initial_client_layout = QGridLayout()
-        self.initial_client_layout.addWidget(self.view_button,0,1)
+        self.main_client_layout = QGridLayout()
+        
         self.initial_client_layout.addWidget(self.search_button,1,0)
         self.initial_client_layout.addWidget(self.edit_button,2,0)
         self.initial_client_layout.addWidget(self.search_line_edit,1,1)
         self.initial_client_layout.addWidget(self.edit_line_edit,2,1)
-        
+
+        self.main_client_layout.addLayout(self.initial_client_layout,1,0)
+        self.main_client_layout.addWidget(self.back_button,2,0)
+        self.main_client_layout.addWidget(self.view_button,0,0)
                                              
         self.view_client_widget = QWidget()
-        self.view_client_widget.setLayout(self.initial_client_layout)
+        self.view_client_widget.setLayout(self.main_client_layout)
+
+        #connections
+        self.back_button.clicked.connect(self.back)
+                                      
+    def create_jobs_layout(self):
+        self.view_button = QPushButton("View Full Database")
+        self.search_button = QPushButton("Search")
+        self.edit_button = QPushButton("Edit An Entry")
+        self.back_button = QPushButton("Back")
+
+        self.search_line_edit = QLineEdit()
+        self.edit_line_edit = QLineEdit()
+
+        self.initial_jobs_layout = QGridLayout()
+        self.main_jobs_layout = QGridLayout()
         
+        self.initial_jobs_layout.addWidget(self.search_button,1,0)
+        self.initial_jobs_layout.addWidget(self.edit_button,2,0)
+        self.initial_jobs_layout.addWidget(self.search_line_edit,1,1)
+        self.initial_jobs_layout.addWidget(self.edit_line_edit,2,1)
+
+        self.main_jobs_layout.addLayout(self.initial_jobs_layout,1,0)
+        self.main_jobs_layout.addWidget(self.back_button,2,0)
+        self.main_jobs_layout.addWidget(self.view_button,0,0)
+                                             
+        self.view_jobs_widget = QWidget()
+        self.view_jobs_widget.setLayout(self.main_jobs_layout)
+
+        #connections
+        self.back_button.clicked.connect(self.back)
+
+    def create_add_layout(self):
+        self.back_button = QPushButton("Back")
+        
+        self.grid = QGridLayout()
+        self.grid.addWidget(self.back_button)
+
+        self.view_add_widget = QWidget()
+        self.view_add_widget.setLayout(self.grid)
+
+        #connections
+        self.back_button.clicked.connect(self.back)
+
+    def create_delete_layout(self):
+        self.delete_button = QPushButton("Delete Entry")
+        self.back_button = QPushButton("Back")
+        self.code_label = QLabel("Customer Code")
+
+        self.delete_line_edit = QLineEdit()
+
+        self.grid1 = QGridLayout()
+        self.grid2 = QGridLayout()
+
+        self.grid1.addWidget(self.code_label,1,0)
+        self.grid1.addWidget(self.delete_line_edit,1,1)
+
+        self.grid2.addLayout(self.grid1,1,0)
+        self.grid2.addWidget(self.delete_button,0,0)
+        self.grid2.addWidget(self.back_button,2,0)
+
+        self.view_delete_widget = QWidget()
+        self.view_delete_widget.setLayout(self.grid2)
+
+        #connections
+        self.back_button.clicked.connect(self.back)
+
+    def create_password_layout(self):
+        self.setWindowTitle("Change Password")
+        
+        self.current_password_label = QLabel("Current Password")
+        self.new_password_label = QLabel("New Password")
+        self.repeat_new_password_label = QLabel("Repeat New Password")
+
+        self.current_password_line_edit = QLineEdit()
+        self.new_password_line_edit = QLineEdit()
+        self.repeat_new_password_line_edit = QLineEdit()
+
+        self.change_password_button = QPushButton("Change Password")
+        self.back_button = QPushButton("Back")
+
+        self.grid1 = QGridLayout()
+        self.grid2 = QGridLayout()
+
+        self.grid1.addWidget(self.current_password_label,0,0)
+        self.grid1.addWidget(self.new_password_label,1,0)
+        self.grid1.addWidget(self.repeat_new_password_label,2,0)
+        self.grid1.addWidget(self.current_password_line_edit,0,1)
+        self.grid1.addWidget(self.new_password_line_edit,1,1)
+        self.grid1.addWidget(self.repeat_new_password_line_edit,2,1)
+
+        self.grid2.addLayout(self.grid1,0,0)
+        self.grid2.addWidget(self.change_password_button,1,0)
+        self.grid2.addWidget(self.back_button,2,0)
+
+        self.view_password_widget = QWidget()
+        self.view_password_widget.setLayout(self.grid2)
+
+        #connections
+        self.back_button.clicked.connect(self.back)
+
     def client(self):
-        print("got here")
         self.create_client_layout()
         self.stacked_layout.addWidget(self.view_client_widget)
         self.stacked_layout.setCurrentIndex(1)   
  
     def jobs_layout(self):
-        print("got here")
+        self.create_jobs_layout()
+        self.stacked_layout.addWidget(self.view_jobs_widget)
+        self.stacked_layout.setCurrentIndex(1)
 
     def add_entry(self):
-        print("got here")
+        pass
+        #self.create_add_layout()
+        #self.stacked_layout.addWidget(self.view_add_widget)
+        #self.stacked_layout.setCurrentIndex(1)
 
     def delete_entry(self):
-        print("got here")
+        self.create_delete_layout()
+        self.stacked_layout.addWidget(self.view_delete_widget)
+        self.stacked_layout.setCurrentIndex(1)
 
     def change_password(self):
-        print("got here")
+        self.create_password_layout()
+        self.stacked_layout.addWidget(self.view_password_widget)
+        self.stacked_layout.setCurrentIndex(1)
+
+    def back(self):
+        self.create_layout()
+        self.stacked_layout.addWidget(self.select_widget)
+        self.stacked_layout.setCurrentIndex(2)
+
        
 def main():
     system = QApplication(sys.argv) #create application
